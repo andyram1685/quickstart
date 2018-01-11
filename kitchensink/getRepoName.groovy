ddedb2d33b58e6a585696692bd264e547c93fc16
Anandaraman <anandaraman85@gmail.com>
2018-01-11 14:58:15 +0800

    Update getRepoName.groovy

--- kitchensink/getRepoName.groovy
+++ kitchensink/getRepoName.groovy
@@ -1,8 +1,9 @@
 // Groovy code to get repo name
 
 def getRepoName(String giturl) {
-    println "${giturl}"
-    return giturl.tokenize('/')[3]
+    println "Git url is ${giturl}"
+    rep_name=giturl.tokenize('/')[3]
+    println "after tokenize ${rep_name}"
     }
 
 return this
